1c1713602cb2c89e8ada299232ff08e51bbaf454
Taras <elouseman@gmail.com>
2020-06-29 23:58:52 +0300

    Initial commit

---
+++ connect.test/Room/RoomViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RoomViewController.swift
+//  connect.test
+//
+//  Created by Тарас Минин on 29/06/2020.
+//  Copyright © 2020 Тарас Минин. All rights reserved.
+//
+
+import UIKit
+
+class RoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
