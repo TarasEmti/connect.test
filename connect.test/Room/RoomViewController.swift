13b1f9e602029b8aaf63805f941da57fde14d314
Taras <elouseman@gmail.com>
2020-07-01 14:15:34 +0300

    Camera output in circle

--- connect.test/Room/RoomViewController.swift
+++ connect.test/Room/RoomViewController.swift
@@ -42,6 +42,16 @@ class RoomViewController: UIViewController {
         return view
     }()
 
+    private lazy var cameraView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.layer.masksToBounds = true
+
+        return view
+    }()
+
+    let renderer = CameraLayerRenderer()
+
     // MARK: - Properties
 
     private let roomFactory = RoomSceneFactory()
@@ -57,6 +67,17 @@ class RoomViewController: UIViewController {
         view.addSubview(scrollView)
 
         presentRoom()
+
+        view.addSubview(cameraView)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        renderer.setupAVCapture()
+        cameraView.layer.addSublayer(renderer.previewLayer)
+        renderer.previewLayer.frame = cameraView.bounds
+        renderer.startSession()
     }
 
     // MARK: - Layout
@@ -66,6 +87,10 @@ class RoomViewController: UIViewController {
 
         scrollView.frame = view.bounds
         roomSceneView.frame = scrollView.bounds
+
+        cameraView.frame = CGRect(origin: .zero, size: CGSize(width: 200, height: 200))
+        cameraView.layer.cornerRadius = 100
+        cameraView.center = view.center
     }
 
     // MARK: - Private methods
@@ -86,4 +111,90 @@ protocol PersonCircular {
     var icon: UIImage { get set }
     var audioSource: String? { get }
     var videoSource: String? { get }
+}
+
+import AVKit
+
+final class CameraLayerRenderer: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    let session = AVCaptureSession()
+    var captureDevice: AVCaptureDevice!
+    var captureQueue: DispatchQueue!
+    var previewLayer: CALayer!
+
+    func checkPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { (isGranted) in
+                if !isGranted {
+                    print("Show error with link to settings")
+                }
+            }
+        case .denied, .restricted:
+            print("Show error with link to settings")
+        case .authorized:
+            break
+        @unknown default:
+            fatalError("handle new AVCaptureDevice authorisation status")
+        }
+    }
+
+    func setupAVCapture() {
+
+        session.sessionPreset = .medium
+        let discovery = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+                                                         mediaType: .video,
+                                                         position: .front)
+        guard let frontalCamera: AVCaptureDevice = discovery.devices.first else {
+            assertionFailure("Device don't have frontal camera")
+            return
+        }
+        captureDevice = frontalCamera
+
+        setupSession()
+    }
+
+    func setupSession() {
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+
+            if session.canAddInput(input) {
+                session.addInput(input)
+            }
+
+            let output = AVCaptureVideoDataOutput()
+            output.alwaysDiscardsLateVideoFrames = true
+
+            captureQueue = DispatchQueue(label: "Video_output")
+            output.setSampleBufferDelegate(self, queue: captureQueue)
+
+            if session.canAddOutput(output) {
+                session.addOutput(output)
+            }
+
+            //output.connection(with: .video)?.isEnabled = true
+            output.connection(with: .video)?.videoOrientation = .portrait
+
+            let layer = AVCaptureVideoPreviewLayer(session: session)
+            layer.videoGravity = .resizeAspectFill
+
+            previewLayer = layer
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func startSession() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.session.startRunning()
+        }
+    }
+
+    func stopSession() {
+        session.stopRunning()
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("what to do here?")
+    }
 }
